af3a1ef38ececfbe755c29eda99549a304763a4f
Celokuhle <celokuhle@psybergate.co.za>
2021-10-05 19:02:56 +0200

    initial after practising

---
+++ src/test/java/za/co/absa/BookOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.absa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

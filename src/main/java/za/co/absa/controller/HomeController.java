819e95ea5fe739a69d0dff839e6a83150e94c7a1
Celokuhle <celokuhle@psybergate.co.za>
2021-10-08 14:17:02 +0200

    made final changes

--- src/main/java/za/co/absa/controller/HomeController.java
+++ src/main/java/za/co/absa/controller/HomeController.java
@@ -9,10 +9,10 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "Welcome to Book Shop Orders API, options available: <br><br>" +
-                "View all: <b>http://localhost:8080/api/orders/all </b><br>" +
-                "Find by ISBN: <b>http://localhost:8080/api/orders/get-by-isbn/{isbn} </b><br>" +
-                "Create: <b>http://localhost:8080/api/orders/place </b><br>" +
-                "--> json body example: { \"isbn\": \"string\",\n" +
+                "View all: <b>~url~/api/orders/all </b><br>" +
+                "Find by ISBN: <b>~url~/api/orders/get-by-isbn/{isbn} </b><br>" +
+                "Create: <b>~url~/api/orders/place </b><br>" +
+                "&emsp; >>> json body example: { \"isbn\": \"string\",\n" +
                 "    \"quantity\": number,\n" +
                 "    \"dateOrdered\": date,\n" +
                 "    \"bookTitle\": \"string\",\n" +
